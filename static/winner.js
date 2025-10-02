8230be465fac907d47c9b13272e68b49f0b4a5c0
prA-G <prdwi2101@gmail.com>
2025-10-02 19:44:52 +0530

    Updated with animation effects

--- static/winner.js
+++ static/winner.js
@@ -1,22 +1,65 @@
 window.onload = function() {
-  // Play winner sound
+  // play win sound
   const winSound = document.getElementById("win-sound");
-  if (winSound) {
+  if (winner !== "Draw" && winSound) {
     winSound.play();
   }
 
-  // Start confetti
+  // trigger confetti
   startConfetti();
+
+  // animate battle
+  animateBattle(p1Choice, p2Choice, winner);
 };
 
-// simple confetti
+function animateBattle(p1, p2, winner) {
+  const p1Img = document.getElementById("p1-img");
+  const p2Img = document.getElementById("p2-img");
+
+  if (winner === "Draw") return;
+
+  if (p1 === "Scissor" && p2 === "Paper") {
+    p1Img.classList.add("attack");
+    setTimeout(() => p2Img.classList.add("hit"), 800);
+    document.getElementById("scissor-sound").play();
+  }
+  else if (p2 === "Scissor" && p1 === "Paper") {
+    p2Img.classList.add("attack");
+    setTimeout(() => p1Img.classList.add("hit"), 800);
+    document.getElementById("scissor-sound").play();
+  }
+
+  else if (p1 === "Paper" && p2 === "Stone") {
+    p1Img.classList.add("attack");
+    setTimeout(() => p2Img.classList.add("hit"), 800);
+    document.getElementById("paper-sound").play();
+  }
+  else if (p2 === "Paper" && p1 === "Stone") {
+    p2Img.classList.add("attack");
+    setTimeout(() => p1Img.classList.add("hit"), 800);
+    document.getElementById("paper-sound").play();
+  }
+
+  else if (p1 === "Stone" && p2 === "Scissor") {
+    p1Img.classList.add("attack");
+    setTimeout(() => p2Img.classList.add("hit"), 800);
+    document.getElementById("stone-sound").play();
+  }
+  else if (p2 === "Stone" && p1 === "Scissor") {
+    p2Img.classList.add("attack");
+    setTimeout(() => p1Img.classList.add("hit"), 800);
+    document.getElementById("stone-sound").play();
+  }
+}
+
+// same confetti function
 function startConfetti() {
   const canvas = document.getElementById('confetti-canvas');
   const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  const pieces = Array.from({length: 150}, () => ({
+  const pieces = Array.from({length: 120}, () => ({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     r: Math.random() * 6 + 4,
